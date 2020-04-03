a2625aec9dabb8341cf2973e06a3af8049d8b5bb
asri52 <asri84368@gmail.com>
2020-04-03 19:16:11 +0000

    adaptive behavior

--- connectGateway.py
+++ connectGateway.py
@@ -3,6 +3,9 @@
 import paho.mqtt.client as mqtt
 import time
 import ssl
+from sense_hat import SenseHat
+
+sense = SenseHat()
 
 host          = "node02.myqtthub.com"
 port          = 8883
@@ -56,53 +59,116 @@ while not client.connected_flag:           #wait in loop
     client.loop()
     time.sleep (1)
 
-# publish message (optionally configuring qos=1, qos=2 and retain=True/False)
-ret = client.publish ("hub-asri84368/mainRoad1", "green")
-print ("MainRoad1 set to green with ret=%s" % ret)
-ret = client.publish ("hub-asri84368/mainRoad2", "green")
-print ("MainRoad2 set to green with ret=%s" % ret)
-ret = client.publish ("hub-asri84368/sideRoad", "red")
-print ("SideRoad set to red with ret=%s" % ret)
+#sensor variables
+sideRoad = False;
+isSpace = False;
+mainRoad1 = False;
+mainRoad2 = False;
 
-sideRoad = input("Please enter the side road value True/False\n")
-isSpace = input("Please enter the space value True/False\n")
+#variables for monitoring frequency of the queue becoming long over 2 mins
+numOfSideRoadTrue = 0;
+s_road_max_time = 120;
+s_road_start_time = 0;
 
-while sideRoad != "exit":
-    if sideRoad=="True":
-        while isSpace != "True":
-            isSpace = input("Please enter the space value True/False\n")
+#variables to time limit adapted system behavior
+mod_max_time = 60;
+mod_start_time = 0;
 
-        while sideRoad=="True":
-            while isSpace=="True":
-                mainRoad1 = input("Please enter the main road 1 road value True/False\n")
-                if mainRoad1 == "True":
-                    ret = client.publish ("hub-asri84368/mainRoad1", "red")
-                    print ("MainRoad1 set to red with ret=%s" % ret)
+def getJoystickDirection():
+    while True: #waiting for min one released event
+           event = sense.stick.wait_for_event(emptybuffer=True)
+           if event.action != "released": #necessary to ignore pressed event action
+               break
+    print(event)
+    print(event.direction)
+    return event.direction
 
-                mainRoad2 = input("Please enter the main road 2 True/False\n")
-                if mainRoad2 == "True":
-                    ret = client.publish ("hub-asri84368/mainRoad1", "red")
-                    print ("MainRoad2 set to red with ret=%s" % ret)
-                    
-                ret = client.publish ("hub-asri84368/sideRoad", "green")
-                print ("SideRoad set to green with ret=%s" % ret)
-                
-                isSpace = input("Please enter the space value True/False\n")
-                
-            isSpace = input("Please enter the space value True/False\n")
-            sideRoad = input("Please enter the side road value True/False\n")
+def setSideRoad():
+    print("Please indicate side road value using joystick, True:move right then press, False: move to any direction but right then press\n")
+    global sideRoad
+    sideRoad = (getJoystickDirection() == "right")
+    print("The value you indicated is" + str(sideRoad))
+    
+def setIsSpace():
+    print("Please enter the space value\n")
+    global isSpace
+    isSpace = getJoystickDirection() == "right"
+    global s_road_start_time
+    global s_road_max_time
+    if isSpace ==True:
+        if (time.time()-s_road_start_time)<s_road_max_time:
+            numOfSideRoadTrue+=1;
+        else:
+            numOfSideRoadTrue=0;
+    print("The value you indicated is" + str(isSpace))
+    if numOfSideRoadTrue<10:
+        while time.time()-mod_start_time<mod_max_time:
+            modified_behavior()
 
+#adapted behavior keeps side road lamps green for 1 min no matter the side road sensor
+def modified_behavior():
+    while isSpace == True:
+        check_main_road()
+        setIsSpace()
+            
+def setMainRoad1():
+    print("Please indicate main road 1 value\n")
+    global mainRoad1
+    mainRoad1 = getJoystickDirection() == "right"
+    print("The value you indicated is" + str(mainRoad1))
+    
+def setMainRoad2():
+    print("Please indicate main road 2 value\n")
+    global mainRoad2
+    mainRoad2 = getJoystickDirection() == "right"
+    print("The value you indicated is" + str(mainRoad2))
+
+def check_main_road():
+    setMainRoad1()
+    if mainRoad1 == True:
+        ret = client.publish ("hub-asri84368/mainRoad1", "red")
+        print ("MainRoad1 set to red")
+    setMainRoad2()
+    if mainRoad2 == True:
+        ret = client.publish ("hub-asri84368/mainRoad1", "red")
+        print ("MainRoad2 set to red")
+    ret = client.publish ("hub-asri84368/sideRoad", "green")
+    print ("SideRoad set to green")
+
+def reset():
+    ret = client.publish ("hub-asri84368/mainRoad1", "green")
+    print ("MainRoad1 set to green")
+    ret = client.publish ("hub-asri84368/mainRoad2", "green")
+    print ("MainRoad2 set to green")
+    ret = client.publish ("hub-asri84368/sideRoad", "red")
+    print ("SideRoad set to red")
+    
+#initialise lamps    
+reset()
+
+#initialise variables
+setSideRoad()
+setIsSpace()
+s_road_start_time = time.time()
+
+while True:
+    if sideRoad == True:
+        while isSpace != True:
+            setIsSpace()
+        while sideRoad == True:           
+            while isSpace == True:
+                check_main_road()
+                setIsSpace()
+                setSideRoad()
+                if sideRoad == False:
+                    break;
+            setIsSpace()
+            setSideRoad()
     else:
-        ret = client.publish ("hub-asri84368/mainRoad1", "green")
-        print ("MainRoad1 set to green with ret=%s" % ret)
-        ret = client.publish ("hub-asri84368/mainRoad2", "green")
-        print ("MainRoad2 set to green with ret=%s" % ret)
-        ret = client.publish ("hub-asri84368/sideRoad", "red")
-        print ("SideRoad set to red with ret=%s" % ret)        
-        
-    client.loop ()
-    sideRoad = input("Please enter the side road value True/False\n")
-    isSpace = input("Please enter the space value True/False\n") 
+        reset()             
+    setSideRoad()
+    setIsSpace()
+    client.loop()
     
 
 # close connection
